eca222331304ed6e5fee217622662eaa9531bbd5
Juan Miguel Z. Marqueses <juanmigzmarq@gmail.com>
2025-01-12 16:45:24 +0700

    Reconfigured issues regarding naming of components on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import {LoginForm} from './components/auth/LoginForm';  // Adjust path if needed
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import "./App.css"
+import LoginForm from "./components/auth/LoginForm" // Adjust path if needed
 //import AdminPage from './components/admin/AdminPage';  // You can create this component for admin
-import {TeacherPage} from './components/teacher/TeacherPage';  // Create this page for teachers
-import {StudentManagement} from './components/teacher/StudentManagement';
+import TeacherPage from "./components/teacher/TeacherPage" // Create this page for teachers
+import StudentManagement from "./components/teacher/StudentManagement"
 //import StudentDashboard from './components/student/StudentDashboard';  // Create this page for students
-import {AdminPage} from "./components/admin/AdminPage";
+import { Admin } from "./components/admin/AdminPage"
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        
-        <Route path="/home" element={<TeacherPage />} />
-        <Route path="/student-management" element={<StudentManagement />} />
-        <Route path="/admin-page" element={<AdminPage />} />
-      
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<LoginForm />} />
+
+				<Route path="/home" element={<TeacherPage />} />
+				<Route path="/student-management" element={<StudentManagement />} />
+				<Route path="/admin-page" element={<Admin />} />
+			</Routes>
+		</Router>
+	)
 }
 
-export default App;
+export default App
